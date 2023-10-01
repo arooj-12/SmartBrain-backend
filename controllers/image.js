c30e1d7979fe730b8a0824688bf1dd3468adfd14
Arooj <aroojshahid1999@gmail.com>
2023-10-01 16:52:55 +0500

    updated

--- controllers/image.js
+++ controllers/image.js
@@ -1,24 +1,15 @@
 const Clarifai = require('clarifai');
 console.log(Clarifai)
 
-const { config } = require('dotenv');
-
-config();
-
-
 const app = new Clarifai.App({
-  apiKey: 'process.env.API_CLARIFAI',
+  apiKey: "TYPE_MY_API_KEY_HERE",
 });
 
 const face_detect_model_obj = {
-  // id: 'face-detection',
-  // name: 'face-detection',
-  // version: '6dc7e46bc9124c5c8824be4822abe105',
-  // type: 'visual-detector',
-    id: process.env.MODEL_ID,
-    name: process.env.MODEL_NAME,
-    version: process.env.MODEL_VERSION,
-    type: process.env.MODEL_TYPE,
+  id: 'face-detection',
+  name: 'face-detection',
+  version: '6dc7e46bc9124c5c8824be4822abe105',
+  type: 'visual-detector',
 }
 
 displayFaceBox = (box) => {
@@ -45,7 +36,9 @@ const handleImage = (req, res, db) => {
     .catch(err => res.status(400).json('unable to get entries'))
   }
 
-  module.exports  = {
-    handleImage: handleImage,
-    handleApiCall: handleApiCall
-}
+  module.exports = {
+    handleImage,
+    handleApiCall
+  }
+
+
--- server.js
+++ server.js
@@ -1,29 +1,24 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); 
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex');
-const { config } = require('dotenv'); 
+const knex = require('knex')
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-config();
-
 const db = knex({
-    client: 'pg',
-    connection: {
-    connectionString: process.env.DATABASE_URL,
-		ssl: { rejectUnauthorized: false },
-		host: process.env.DATABASE_LOCALHOST,
-		port: 5432,
-		user: process.env.DATABASE_USER,
-		password: process.env.DATABASE_PASSWORD,
-		database: process.env.DATABASE_NAME
-    }
-  });
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    port: 5432,
+    user: 'postgres',
+    password : 'test',
+    database : 'smart-brain'
+  }
+});
 
 
 const app = express();
@@ -38,6 +33,7 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3001, ()=> {
-  console.log(`app is running on port ${process.env.PORT}`);
+app.listen(3001, ()=> {
+  console.log('app is running on port 3001');
 })
+
